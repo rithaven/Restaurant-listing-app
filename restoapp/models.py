67f3970fcc80b310b25c9a6d899735bfba9bccac
rithaven <veneruth4@gmail.com>
2020-01-09 17:23:57 +0200

    Add static file

--- restoapp/models.py
+++ restoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name= models.Charfield(max_length=30)
+     
+    def __str__(self):
+        return self.name
+    def saveLocation(self):
+        self.save()
+
+    def deleteLocation(self):
+        Location.objects.filter(id = self.pk).delete()
+    
+    def updateLocation(self, **kwargs):
+        self.objects.filter(id= self.pk).update(**kwargs)
+    
