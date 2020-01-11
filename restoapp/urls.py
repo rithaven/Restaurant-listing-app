d3eb456637247ac3f0cc7db33a7e46c2ab0f4b5a
rithaven <veneruth4@gmail.com>
2020-01-11 16:42:21 +0200

    Add bootstrap4

---
+++ restoapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$', views.resto,name='resto'),
+    url(r'^locations/', views.resto_location,name='location'),
+    url(r'search/',views.search_resto,name='search_resto')
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- restoproject/settings.py
+++ restoproject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'restoapp.apps.RestoapConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
