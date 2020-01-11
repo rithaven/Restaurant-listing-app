0e536a91c886642b9cb423c8814d310426e9ef21
rithaven <veneruth4@gmail.com>
2020-01-11 17:00:14 +0200

    Add resto function

--- restoapp/views.py
+++ restoapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404
+from .models import Image
 # Create your views here.
+def resto(request):
+    restaurent=Resto.restaurent()
+    return render (request, 'restaurents.html',{"restaurent":restaurent})
